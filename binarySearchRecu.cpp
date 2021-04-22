c83ad97bbf8fdcc860d4e8d26591424eceda1f11
InYuusha <asuse975@gmail.com>
2021-04-22 07:04:03 +0530

    binarySeach recursive

---
+++ binarySearchRecu.cpp
@@ -0,0 +1,33 @@
+#include <iostream>
+using namespace std;
+
+int binarySearch(int arr[],int low,int high,int key){
+      int mid;
+    if(low<=high){
+        mid =(low+high)/2;
+        if(key==arr[mid]){
+            return mid;
+        }
+        else {
+            if(key<arr[mid]){
+                return binarySearch(arr,low,mid-1,key);
+            }
+            else {
+                if(key>arr[mid]){
+                    return binarySearch(arr,mid+1,high,key);
+                }
+            }
+        }
+       
+    }
+    else return -1 ;
+    
+}
+int main(){
+    int arr[5]={2,5,7,9,11};
+    int res = binarySearch(arr,0,4,5);
+    cout<<res;
+    return 0;
+
+    
+}
